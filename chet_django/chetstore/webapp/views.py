5527be841b2abebe9bc1842dd464292819141f2a
Alexander Tzoc <jealtzo26@gmail.com>
2021-06-28 12:53:13 -0600

    Adding JS libray codemirror

--- chet_django/chetstore/webapp/views.py
+++ chet_django/chetstore/webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def codemirror(request):
+    if request.method == 'POST'    :
+        print('*'*10)
+        data = request.POST['data']
+        print(data)
+        print('*'*10)
+    return render(request, 'codemirror.html')
